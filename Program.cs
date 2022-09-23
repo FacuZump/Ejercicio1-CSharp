5b5ee06326c9021eb9b9f433482bf53733a8a87b
Facundo Nicolás <facundozumpano@gmail.com>
2022-09-23 12:50:03 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+
+
 Console.WriteLine("Hola, escribe tu nombre");
 string nombre = Console.ReadLine();
 Console.WriteLine($"Tu nombre es {nombre}");
+
+
+Console.WriteLine("Hola, escribe la hora");
+float hora = float.Parse(Console.ReadLine());
+Console.WriteLine($"La hora es {hora}");
