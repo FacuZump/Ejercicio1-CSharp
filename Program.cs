8c940eca71ccda7afa41521b47d2753bb21e8a30
facun <facun@FZLAPTOP>
2022-09-23 12:36:35 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hola, escribe tu nombre");
+string nombre = Console.ReadLine();
+Console.WriteLine($"Tu nombre es {nombre}");
